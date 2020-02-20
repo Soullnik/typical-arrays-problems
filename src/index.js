796bc5cc1f6d7bed2b5eb6d3c84d60d6371cf5c9
Soullnik <soullnik96@gmail.com>
2020-02-21 00:26:32 +0300

    feat: test completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    return array.reduce((min, num ) => (num < min) ? num : min , 0)
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) return 0;
+    
+    return array.reduce((max, num ) => (num > max) ? num : max , 0)
+    
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(!array || array.length === 0) return 0;
+    return array.reduce((sum, num) => sum + num, 0) / array.length;
+  }
